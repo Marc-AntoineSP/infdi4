f3b571c8acf47aff499ab5a2c7c4495edd4f6433
Marc-AntoineSP <marcantoine.saintpierre@viacesi.fr>
2026-04-27 11:35:32 +0200

    :bug: PCS-6 fix Error file

--- Core/Error.php
+++ Core/Error.php
@@ -2,6 +2,10 @@
 
 namespace Core;
 
+use App\Config;
+use ErrorException;
+use Exception;
+
 /**
  * Error and exception handler
  *
@@ -13,17 +17,18 @@ class Error
     /**
      * Error handler. Convert all errors to Exceptions by throwing an ErrorException.
      *
-     * @param int $level  Error level
-     * @param string $message  Error message
-     * @param string $file  Filename the error was raised in
-     * @param int $line  Line number in the file
+     * @param int $level Error level
+     * @param string $message Error message
+     * @param string $file Filename the error was raised in
+     * @param int $line Line number in the file
      *
      * @return void
+     * @throws ErrorException
      */
     public static function errorHandler($level, $message, $file, $line)
     {
         if (error_reporting() !== 0) {  // to keep the @ operator working
-            throw new \ErrorException($message, 0, $level, $file, $line);
+            throw new ErrorException($message, 0, $level, $file, $line);
         }
     }
 
@@ -36,14 +41,13 @@ class Error
      */
     public static function exceptionHandler($exception)
     {
-        // Code is 404 (not found) or 500 (general error)
         $code = $exception->getCode();
-        if ($code != 404) {
+        if ($code !== 404) {
             $code = 500;
         }
         http_response_code($code);
 
-        if (\App\Config::SHOW_ERRORS) {
+        if (Config::SHOW_ERRORS) {
             echo "<h1>Fatal error</h1>";
             echo "<p>Uncaught exception: '" . get_class($exception) . "'</p>";
             echo "<p>Message: '" . $exception->getMessage() . "'</p>";
