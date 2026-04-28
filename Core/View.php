46599ffdfa22cf4c0f8f9d199866f5a22dc4b364
Marc-AntoineSP <marcantoine.saintpierre@viacesi.fr>
2026-04-28 04:36:46 +0200

    :rotating_light: PCS-15 Removed redundant render method, use renderTemplate instead

--- Core/View.php
+++ Core/View.php
@@ -19,24 +19,14 @@ class View
 {
 
     /**
-     * Render a view file
-     *
-     * @param string $view  The view file
+     * @param string $view  The template file
      * @param array $args  Associative array of data to display in the view (optional)
      *
      * @return void
      */
-    public static function render($view, $args = [])
+    public static function render(string $view, array $args = []): void
     {
-        extract($args, EXTR_SKIP);
-
-        $file = dirname(__DIR__) . "/App/Views/$view";  // relative to Core directory
-
-        if (is_readable($file)) {
-            require_once $file;
-        } else {
-            throw new RuntimeException("$file not found");
-        }
+        self::renderTemplate($view, $args);
     }
 
     /**
